34d18e447a6ff35379af87042a9ed791bc9ef517
brettcutt <brettcutt@outlook.com.au>
2019-02-20 11:33:01 +1030

    add filtering for price range and rating

--- static/js/filter.js
+++ static/js/filter.js
@@ -10,33 +10,78 @@ function filterRestaurants() {
         }
     )
 
-    var checkedCuisine = []
+    var filterByCuisine = filterCuisine(restaurants);
+
+    var filteredByPrice = filterPrice(filterByCuisine);
+
+    var filteredByRating = filterRating(filteredByPrice);
+
+    document.getElementById('restaurant-list').innerHTML = getRestaurantList(filteredByRating)
+
+}
+
+// Checkbox filter. What ever cuisine checkboxes are selected, they are then matched with
+// the restaurants that have the same cuisine type.
+function filterCuisine(restaurants) {
+    console.log(restaurants)
+    var checkedCuisine = [];
     $("input:checkbox[name=cuisine-checkbox]:checked").each(function () {
         checkedCuisine.push($(this).val());
     });
-    getAmountValues(amountValues)
-    getRatingValues(ratingValues)
-    console.log(checkedCuisine)
 
-    // Checkbox filter. What ever cuisine checkboxes are selected, they are then matched with
-    // the restaurants that have the same cuisine type.
-    var result = [];
-    restaurants.forEach(function (restaurant) {
-        var restaurantCuisines = restaurant.restaurant.cuisines.split(", ")
-        restaurantCuisines
-        console.log('test', restaurantCuisines)
-        restaurantCuisines.forEach(function (cuisine) {
-            if (checkedCuisine.includes(cuisine)) {
-                result.push(restaurant)
+    var filterByCuisine = [];
+    if (checkedCuisine != "") {
 
-            }
-
+        restaurants.forEach(function (restaurant) {
+            var restaurantCuisines = restaurant.restaurant.cuisines.split(", ")
+            restaurantCuisines
+            restaurantCuisines.forEach(function (cuisine) {
+                if (checkedCuisine.includes(cuisine)) {
+                    filterByCuisine.push(restaurant)
+                }
+            })
         })
+    } else {
+        filterByCuisine = restaurants;
+    }
+    return filterByCuisine
+}
+
+// Filter Restaurant by its price
+function filterPrice(filterByCuisine) {
+
+    var priceRange = getAmountValues()
+
+    var filteredByPrice = []
+
+    filterByCuisine.forEach(function (restaurant) {
+
+        var restaurantPrice = restaurant.restaurant.price_range
+
+        if (restaurantPrice >= priceRange[0] && restaurantPrice <= priceRange[1]) {
+            filteredByPrice.push(restaurant)
+        }
     })
 
-    document.getElementById('restaurant-list').innerHTML = getRestaurantList(result)
+    return filteredByPrice
+}
 
+// Filter Restaurant by its price
+function filterRating(filteredByPrice) {
 
-    console.log("results", result)
+    // Filter Restaurant by its rating
+    var ratingRange = getRatingValues()
 
+    var filteredByRating = []
+
+    filteredByPrice.forEach(function (restaurant) {
+
+        var restaurantRating = restaurant.restaurant.user_rating.aggregate_rating;
+
+        if (restaurantRating >= ratingRange[0] && restaurantRating <= ratingRange[1]) {
+            filteredByRating.push(restaurant)
+        }
+    })
+
+    return filteredByRating;
 }
