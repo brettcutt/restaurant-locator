17fb87d050fe5bd7e37c7aea19036150ad2fdbcd
brettcutt <brettcutt@outlook.com.au>
2019-02-21 12:44:33 +1030

    put cuisine and category creation in one function instead of two, due to error

---
+++ static/js/checkboxes.js
@@ -0,0 +1,31 @@
+function createCheckbox() {
+
+    $.when(
+        $.getJSON('checkboxes.json')
+    ).then(
+        function (response) {
+            // Category checkboxes
+            var categories = response.categories
+            var result = []
+            categories.forEach(function (category) {
+                result.push(`<li><input name='category-checkbox' class='category-checkbox' oninput="filterRestaurants()" type="checkbox" value='${category.categories.id}'>${category.categories.name}</li>`)
+            })
+
+            result = result.join("")
+            document.getElementById('category-search').innerHTML = result;
+
+
+            // Cuisine checkboxes
+            var cuisines = response.cuisines
+            var result = []
+            cuisines.forEach(function (cuisine) {
+                result.push(`<li><input name='cuisine-checkbox' class='cuisine-checkbox' oninput="filterRestaurants()" type="checkbox" value='${cuisine.cuisine.cuisine_id}'>${cuisine.cuisine.cuisine_name}</li>`)
+            })
+
+            result = result.join("")
+            document.getElementById('cuisine-search').innerHTML = result
+        }
+    )
+}
+
+createCheckbox()
