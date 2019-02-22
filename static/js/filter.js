91e938be5defa49c81f87accb67fe712d830be51
brettcutt <brettcutt@outlook.com.au>
2019-02-22 12:05:36 +1030

    adding page fading and a keyword search

--- static/js/filter.js
+++ static/js/filter.js
@@ -1,29 +1,33 @@
-
 function filterRestaurants() {
 
-    // calls the function to filter the cuisines selected in the cuisines checkboxes
-    var cuisineString = filterCuisine()
+    // Returns a string with cuisine values and formatted to be read by the api url.
+    var cuisineString = filterCuisine();
 
-    // calls the function to filter the categories selected in the cuisines checkboxes
-    var categoryString = filterCategory()
+    // Returns a string with category values and formatted to be read by the api url.
+    var categoryString = filterCategory();
 
-    var apiString = apiUrl + apiKey + cuisineString + categoryString
+    // Returns a string with a keyword and formatted to be read by the api url.
+    var keywordString = keywordSearch();
+
+    // the api combination to query the Zomato api database
+    var apiString = apiUrl + apiKey + cuisineString + categoryString + keywordString;
     $.when(
         $.getJSON(apiString)
     ).then(
         function (response) {
-            restaurants = response.restaurants
+            // restaurantData is the global varaible from restaurant-api.js
+            // all function have access to retrieved restaurant data.
+            restaurantData = response.restaurants;
 
-            // calls the function to filter by the pricing slider
-            var filteredByPrice = filterPrice(restaurants);
+            // fade hide and show the restaurant-list element 
+            $('#restaurant-list').hide('fade', 500);
+            setTimeout(
+                function () {
+                    $('#restaurant-list').show('fade', 500);
 
-            // calls the function to filter by the rating slider
-            var filteredByRating = filterRating(filteredByPrice);
-
-            // restaurantData on line 3 in restaurant-api.js
-            restaurantData = filteredByRating;
-
-            document.getElementById('restaurant-list').innerHTML = getRestaurantList(restaurantData)
+                    // Adds a list of restaurants to #restaurant-list(ul)
+                    document.getElementById('restaurant-list').innerHTML = getRestaurantList(restaurantData);
+                }, 500);
         }
     )
 
@@ -32,7 +36,7 @@ function filterRestaurants() {
 // and creates a string that appends to the new api url.
 // eg. &cuisines=969%2C147%2C84, &cusisines= Modern Australia, Morocan, Pizza 
 function filterCuisine() {
-    var cuisineString = ""
+    var cuisineString = "";
 
     $("input:checkbox[name=cuisine-checkbox]:checked").each(function () {
         if (cuisineString.length == 0) {
@@ -43,7 +47,7 @@ function filterCuisine() {
         }
     });
 
-    return cuisineString
+    return cuisineString;
 }
 
 // Filter Restaurant by its category. This grabs all the category id values from the checked boxes
@@ -59,44 +63,51 @@ function filterCategory() {
             categoryString += "%2C" + $(this).val();
         }
     });
-    return categoryString
+    return categoryString;
 }
 
 // Filter Restaurant by its price
-function filterPrice(filterByCuisine) {
-    var priceRange = getAmountValues()
+function filterPrice() {
+    var priceRange = getAmountValues();
 
-    var filteredByPrice = []
+    var filteredByPrice = [];
 
-    filterByCuisine.forEach(function (restaurant) {
+    restaurantData.forEach(function (restaurant) {
 
-        var restaurantPrice = restaurant.restaurant.price_range
+        var restaurantPrice = restaurant.restaurant.price_range;
 
         if (restaurantPrice >= priceRange[0] && restaurantPrice <= priceRange[1]) {
-            filteredByPrice.push(restaurant)
+            filteredByPrice.push(restaurant);
         }
     })
-
-    return filteredByPrice
+    document.getElementById('restaurant-list').innerHTML = getRestaurantList(filteredByPrice);
+    // return filteredByPrice
 }
 
 // Filter Restaurant by its price
-function filterRating(filteredByPrice) {
+function filterRating() {
 
     // Filter Restaurant by its rating
-    var ratingRange = getRatingValues()
+    var ratingRange = getRatingValues();
 
-    var filteredByRating = []
+    var filteredByRating = [];
 
-    filteredByPrice.forEach(function (restaurant) {
+    restaurantData.forEach(function (restaurant) {
 
         var restaurantRating = restaurant.restaurant.user_rating.aggregate_rating;
 
         if (restaurantRating >= ratingRange[0] && restaurantRating <= ratingRange[1]) {
-            filteredByRating.push(restaurant)
+            filteredByRating.push(restaurant);
         }
     })
+    document.getElementById('restaurant-list').innerHTML = getRestaurantList(filteredByRating);
+    // return filteredByRating;
+}
 
-    return filteredByRating;
+// Filter Restaurant by a keyword
+function keywordSearch() {
+    var keyword = "&q=" + document.getElementById('keyword-search').value;
+
+    return keyword;
 }
 
